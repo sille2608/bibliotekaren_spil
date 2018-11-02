b18f547cce38bfc146c0e2bbda05fe9fa1017203
Sille Agerskov Christensen <sillechristensen@Silles-MacBook-Pro.local>
2018-11-02 12:47:54 +0100

    mission bobbel og bøger implementeret

--- script.js
+++ script.js
@@ -15,17 +15,18 @@ function showStart() {
     console.log("showStart");
     //vis startskærm
     document.querySelector("#start").classList.add("show");
+
     //start animation på start-knap
     document.querySelector("#play").classList.add("pulse");
 
-    //At der kan klikkes på play
+    //klik for at komme til mission skærm
     document.querySelector("#play").addEventListener("click", hideStart);
 
-    //At der kan klikkes på setting
-    document.querySelector("#settings_bottom").addEventListener("click", showSetting);
+    //klik for at komme til setting skærm
+    document.querySelector("#settings_buttom").addEventListener("click", showSetting);
 }
 
-function showSetting (){
+function showSetting() {
     console.log("showSetting");
 
     //vis setting skræm
@@ -41,46 +42,79 @@ function hideSetting() {
 
     //skjul setting skærm
     document.querySelector("#settings").classList.remove("show");
-
 }
 
 function hideStart() {
     console.log("hideStart");
     //stop animation på start-knap
     document.querySelector("#play").classList.remove("pulse");
+
+    //bøgerne der kan klikkes på, sættes ind og aktiveres
+    document.querySelector("#bog_sommerfugl").classList.add("foer_klik");
+    document.querySelector("#bog_sol").classList.add("foer_klik");
+    document.querySelector("#bog_hund").classList.add("foer_klik");
+    document.querySelector("#bog_elefant").classList.add("foer_klik");
+
+    document.querySelector("#bog_gris").classList.add("foer_klik");
+    document.querySelector("#bog_faar").classList.add("foer_klik");
+    document.querySelector("#bog_ko").classList.add("foer_klik");
+    document.querySelector("#bog_kat").classList.add("foer_klik");
+
+
     //fade startskærm ud
     document.querySelector("#start").classList.add("fade_out");
 
+    //siden sendes videre til show mission
+    document.querySelector("#start").addEventListener("animationend", showMission);
+
 }
 
-function gameScreen() {
-    console.log("gameScreen");
+function showMission() {
+    console.log("showMission");
 
     //skjul startskærm
     document.querySelector("#start").classList.add("hide");
+    document.querySelector("#start").classList.remove("show");
+
     //vis spilskærm
-    document.querySelector("#game").classList.add("show");
+    document.querySelector("#game_background").classList.add("show");
 
-
-
-}
-
-
-function gameMission() {
-    console.log("gameMission");
     //vis talebobel med spillets regler
-    //NOTE: DET VED JEG IKKE LIGE
+    document.querySelector("#mission").classList.add("show");
+
+    //vis mission play knappen
+    document.querySelector("#mission_play").classList.add("show");
+
+
+    //start animation på play knappen
+    document.querySelector("#mission_play").classList.add("pulse");
+
+
+    //Klik til spil start
+    document.querySelector("#mission_play").addEventListener("click", hideMission);
+
 }
 
 
 function hideMission() {
     console.log("hideMission");
+
+    //stop animation på start spil knappen
+    document.querySelector("#mission_play").classList.remove("pulse");
+
+    //fade out Mission-talebobbel
+    document.querySelector("#mission").classList.add("fade_out");
+
     //skjul Mission-talebobbel
-    //NOTE: DET VED JEG IKKE LIGE
+    document.querySelector("#mission").classList.add("hide");
+
+    //skjul mission play knap
+    document.querySelector("#mission_play").classList.remove("show");
 
     //når skjul Mission-talebobbel er færdig -startGame
-    document.querySelector("#start").addEventListener("animationend");
+    document.querySelector("#mission").addEventListener("animationend", startGame);
 }
+
 
 function startGame() {
     console.log("startGame");
@@ -88,10 +122,28 @@ function startGame() {
     //start tiden
     //NOTE: DET VED JEG IKKE LIGE
 
+    // start animation på ur-viseren
+    document.querySelector("#time").classList.add("time_rotation");
+
+    //aktiver at der kan klikkes på bøgerne
+    document.querySelector("#bog_sommerfugl").addEventListener("click", clickWrong);
+    document.querySelector("#bog_sol").addEventListener("click", clickWrong);
+    document.querySelector("#bog_hund").addEventListener("click", clickWrong);
+    document.querySelector("#bog_elefant").addEventListener("click", clickWrong);
+
+    document.querySelector("#bog_gris").addEventListener("click", clickCorrect);
+    document.querySelector("#bog_faar").addEventListener("click", clickCorrect);
+    document.querySelector("#bog_ko").addEventListener("click", clickCorrect);
+    document.querySelector("#bog_kat").addEventListener("click", clickCorrect);
+
+
 }
 
 function clickCorrect() {
     console.log("clickCorrect")
+
+    this.classList.remove("foer_klik");
+    this.classList.add("efter_klik");
 
     //vis grøn bog
     //NOTE: DET VED JEG IKKE LIGE
@@ -105,6 +157,9 @@ function clickCorrect() {
 
 function clickWrong() {
     console.log("clickWrong")
+
+    this.classList.remove("foer_klik");
+    this.classList.add("efter_klik");
 
     //vis grøn bog
     //NOTE: DET VED JEG IKKE LIGE
